4883afc3e765a8c43344aefce277abd6b8ab7be1
rc2pc2 <petriccarcc@gmail.com>
2023-07-19 12:39:21 +0200

    added custom footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,3 +1,29 @@
 <footer>
-
+    <div class="footer-brand">
+        <img src="{{  Vite::asset('resources/img/marchio-sito-test.png') }}" alt="La Molisana logo">
+    </div>
+    <nav class="pastas-footer">
+        <h2>
+            Pastificio
+        </h2>
+        <ul>
+            <li><a href="#">Il Pastificio</a></li>
+            <li><a href="#">Grano</a></li>
+            <li><a href="#">Filiera</a></li>
+            <li><a href="#">100 anni di pasta</a></li>
+            <li><a href="#">Sartoria della pasta</a></li>
+        </ul>
+    </nav>
+    <nav class="products-footer">
+        <h2>
+            Prodotti
+        </h2>
+        <ul>
+            <li><a href="#">Le classiche</a></li>
+            <li><a href="#">Le Integrali</a></li>
+            <li><a href="#">Le Speciali</a></li>
+            <li><a href="#">Le Biologiche</a></li>
+            <li><a href="#">Le Gluten-free</a></li>
+        </ul>
+    </nav>
 </footer>
